d031351fe13ff50a474aef18ca05eb232a0eb360
pighwan21 <pighwan21@gmail.com>
2022-12-10 00:07:31 +0900

    pig

--- src/Pig.java
+++ src/Pig.java
@@ -1,3 +1,5 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -5,52 +7,54 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-import javax.imageio.ImageIO;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-
 public class Pig extends JFrame{
-    int count = 0;
-
+    BufferedImage pig = null;
     Pig() {
 
-    // Button b1
-    JButton b1 = new JButton("Pig~");
-    b1.setBounds(100, 150, 100, 30);
+    // Button b1(이미지 파일을 그레이 스케일로 변환)
+    JButton b1 = new JButton("[그레이 스케일]");
+    b1.setBounds(120, 80, 150, 30);
     b1.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
-            BufferedImage pig = null;
+            // 이미지 불러오기
             try {
                 pig = ImageIO.read(new File("C:\\\\Users\\\\USER\\\\Documents\\\\gom.jpg"));
             } catch (IOException e1) {
                 System.out.println(e1.toString());
             }
-
-
+            // 그레이스케일로 변환하기
             for (int y = 0; y < pig.getHeight(); y++) {
                 for (int x = 0; x < pig.getWidth(); x++) {
                     Color colour = new Color(pig.getRGB(x, y));
-//       Choose one from below
-//       int Y = (int) (0.299 * colour.getRed() + 0.587 * colour.getGreen() + 0.114 * colour.getBlue());
                     int Y = (int) (0.2126 * colour.getRed() + 0.7152 * colour.getGreen() + 0.0722 * colour.getBlue());
                     pig.setRGB(x, y, new Color(Y, Y, Y).getRGB());
                 }
             }
-
+        }
+    });
+    // button2(저장)
+    JButton b2 = new JButton("[저장하기]");
+    b2.setBounds(120, 160, 150, 30);
+    b2.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // 변환한 이미지를 gray.jpg로 저장하기
             try {
-                ImageIO.write(pig, "jpg", new File("C:\\Users\\USER\\Documents\\gom2.jpg"));
+                ImageIO.write(pig, "jpg", new File("C:\\Users\\USER\\Documents\\gray.jpg"));
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
         }
     });
-    add(b1);
-    setTitle("JButton EX");
-    setSize(500,400);
-    setLayout(null);
-    setVisible(true);
+         add(b1); add(b2);
+         setTitle("JButton 숙제");
+         setSize(400,300);
+         setLayout(null);
+         setVisible(true);
     }
+
+    // 실행
     public static void main(String[] args) {
         new Pig();
     }
--- src/Pig2.java
+++ src/Pig2.java
@@ -1,5 +1,3 @@
-import javax.imageio.ImageIO;
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,14 +5,18 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 public class Pig2 extends JFrame{
     int count = 0;
 
     Pig2() {
 
     // Button b1
-    JButton b1 = new JButton("[그레이 스케일]");
-    b1.setBounds(100, 150, 200, 30);
+    JButton b1 = new JButton("Pig~");
+    b1.setBounds(100, 150, 100, 30);
     b1.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
@@ -25,52 +27,30 @@ public class Pig2 extends JFrame{
                 System.out.println(e1.toString());
             }
 
-            for (int y = 0; y < pig.getHeight(); y++) {
-                for (int x = 0; x < pig.getWidth(); x++) {
-                    Color colour = new Color(pig.getRGB(x, y));
-                    int Y = (int) (0.2126 * colour.getRed() + 0.7152 * colour.getGreen() + 0.0722 * colour.getBlue());
-                    pig.setRGB(x, y, new Color(Y, Y, Y).getRGB());
-                }
-            }
-        }
-    });
-
-    JButton b2 = new JButton("[저장하기]");
-    b2.setBounds(300, 150, 100, 30);
-    b2.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-
-            BufferedImage pig = null;
-            try {
-                pig = ImageIO.read(new File("C:\\\\Users\\\\USER\\\\Documents\\\\gom.jpg"));
-            } catch (IOException e1) {
-                System.out.println(e1.toString());
-            }
 
             for (int y = 0; y < pig.getHeight(); y++) {
                 for (int x = 0; x < pig.getWidth(); x++) {
                     Color colour = new Color(pig.getRGB(x, y));
+//       Choose one from below
+//       int Y = (int) (0.299 * colour.getRed() + 0.587 * colour.getGreen() + 0.114 * colour.getBlue());
                     int Y = (int) (0.2126 * colour.getRed() + 0.7152 * colour.getGreen() + 0.0722 * colour.getBlue());
                     pig.setRGB(x, y, new Color(Y, Y, Y).getRGB());
                 }
             }
 
             try {
-                ImageIO.write(pig, "jpg", new File("C:\\Users\\USER\\Documents\\gray.jpg"));
+                ImageIO.write(pig, "jpg", new File("C:\\Users\\USER\\Documents\\gom2.jpg"));
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
         }
     });
-         add(b1); add(b2);
-         setTitle("JButton 숙제");
-         setSize(500,400);
-         setLayout(null);
-         setVisible(true);
-
+    add(b1);
+    setTitle("JButton EX");
+    setSize(500,400);
+    setLayout(null);
+    setVisible(true);
     }
-
     public static void main(String[] args) {
         new Pig2();
     }
